bc267e35ce991fde2d114f78c05c20d650a96c5a
jaharzli <joumenali.harzli@externe.mousquetaires.com>
2018-12-11 14:36:13 +0100

    Update Service and Controller

---
+++ notifications-service/app/Filters.scala
@@ -0,0 +1,10 @@
+import javax.inject.Inject
+import play.api.http.DefaultHttpFilters
+import play.filters.gzip.GzipFilter
+
+/**
+  * Filters
+  *
+  * @author jaharzli
+  */
+class Filters @Inject() (gzipFilter: GzipFilter) extends DefaultHttpFilters(gzipFilter)
